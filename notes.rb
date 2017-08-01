8cae9b71cf7802fe317f34c9fad00009bc911520
EJ Vickery <email": null>
2017-08-01 10:16:34 -0700

    Initial commit

---
+++ notes.rb
@@ -0,0 +1,67 @@
+## jQuery / AJAX examples
+
+# Regular jQuery
+$('.post-container').on('click', '.upvote-button', function(e){
+  e.preventDefault();
+  var $this = $(this);
+  var $url = $this.closest('form').attr('action');
+  var $points = $this.closest('article').find('.points');
+
+  $this.addClass('red');
+
+  $.ajax({
+    method: 'POST',
+    url: $url
+  }).done(function(response){
+    $points.text(response);
+  });
+});
+
+# jQuery with JSON
+
+$('.post-container').on('click', '.upvote-button', function(e){
+  e.preventDefault();
+  var $this = $(this);
+  var $url = $this.closest('form').attr('action');
+  var $points = $this.closest('article').find('.points');
+
+  $this.addClass('red');
+
+  $.ajax({
+    method: 'POST',
+    dataType: "json",
+    url: $url
+  }).done(function(response){
+    $points.text(response.votes);
+  });
+});
+
+# ---------------------------------- #
+## Post route AJAX examples
+
+# Without JSON
+post '/posts/:id/vote' do
+  post = Post.find(params[:id])
+  post.votes.create(value: 1)
+  if request.xhr?
+    post.points.to_s
+  else
+    redirect "/posts"
+  end
+end
+
+# With JSON
+post '/posts/:id/vote' do
+  post = Post.find(params[:id])
+  post.votes.create(value: 1)
+  if request.xhr?
+    content_type :json
+    {post: post.id, votes: post.points.to_s}.to_json
+  else
+    redirect "/posts"
+  end
+end
+
+## Time tests
+OOJS (Bikes and Stations): 16 mins
+OOJS (Gardens): 22 mins
